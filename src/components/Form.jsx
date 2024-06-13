78950805aaf0d05d8b226c418c82bbd1d15c761d
svivek19 <svivek030503@gmail.com>
2024-06-13 12:16:35 +0530

    feat: direct emailjs integration

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+
   const handleBtn = (e) => {
     e.preventDefault();
+    const service = import.meta.env.VITE_SERVICE_ID;
+    const template = import.meta.env.VITE_TEMPLATE_ID;
+    const publickey = import.meta.env.VITE_PUBLIC_KEY;
+
+    emailjs
+      .sendForm(service, template, form.current, {
+        publicKey: publickey,
+      })
+      .then(() => {
+        console.log("success");
+      })
+      .catch((err) => {
+        console.log(err.text);
+      });
   };
 
   return (
@@ -11,20 +28,23 @@ const Form = () => {
       data-aos="fade-left"
       data-aos-duration="3000"
     >
-      <form className="flex flex-col space-y-2">
+      <form className="flex flex-col space-y-2" ref={form}>
         <label>Name</label>
         <input
           type="text"
+          name="user_name"
           className="border border-slate-300 p-1 rounded px-2 text-slate-700 outline-sky-500"
         />
         <label>Email Id</label>
         <input
           type="email"
+          name="user_email"
           className="border border-slate-300 p-1 rounded px-2 text-slate-700 outline-sky-500"
         />
         <label>Phone number</label>
         <input
           type="number"
+          name="user_phone"
           className="border border-slate-300 p-1 rounded px-2 text-slate-700 outline-sky-500"
         />
         <label>Message</label>
@@ -32,6 +52,7 @@ const Form = () => {
           cols="20"
           rows="5"
           placeholder="Feel free to ask!"
+          name="user_message"
           className="border border-slate-300 p-1 rounded px-2 text-slate-700
         outline-sky-500"
         ></textarea>
