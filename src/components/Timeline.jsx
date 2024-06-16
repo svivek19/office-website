1003cbf3f5f4dfa676ffd31fb013945c5f491fd5
svivek19 <svivek030503@gmail.com>
2024-06-16 10:08:36 +0530

    product page complete, feat: dialog model for product overview, benefits and features

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+
 import { AiFillAlert } from "react-icons/ai";
 import { GiTechnoHeart, GiPeaceDove } from "react-icons/gi";
 import { CgSmileMouthOpen } from "react-icons/cg";
@@ -13,7 +17,6 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 const Timeline = ({ data }) => {
-  // Define arrays for icons and background colors
   const icons = [
     AiFillAlert,
     GiTechnoHeart,
@@ -46,15 +49,54 @@ const Timeline = ({ data }) => {
             <h3 className="vertical-timeline-element-title text-xl font-medium">
               {item.title}
             </h3>
-            <p className="text-slate-500">
+            <div className="text-slate-500">
               {item.overview.length > 20
                 ? item.overview.slice(0, 70) + "..."
                 : item.overview}
-            </p>
+            </div>
 
-            <button className="text-violet-700 underline my-3">
-              Read more
-            </button>
+            {/* dialog model*/}
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <button className="text-violet-700 underline my-3">
+                  Read more
+                </button>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0 z-50" />
+                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 max-h-[80vh] w-full max-w-[450px] rounded-[6px] bg-white p-[25px] shadow-lg focus:outline-none">
+                  <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+                    {item.title}
+                  </Dialog.Title>
+                  <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal text-justify">
+                    {item.overview}
+                  </Dialog.Description>
+
+                  <div className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal text-justify">
+                    <ul>
+                      <li className="font-semibold">Features</li>
+                      {item.features.map((feat, i) => (
+                        <li key={i}>{feat}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal text-justify">
+                    <h2 className="font-semibold">Benefits</h2>
+                    <p>{item.benefits}</p>
+                  </div>
+
+                  <Dialog.Close asChild>
+                    <button
+                      className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                      aria-label="Close"
+                    >
+                      <Cross2Icon />
+                    </button>
+                  </Dialog.Close>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           </VerticalTimelineElement>
         );
       })}
---
+++ src/components/model/DialogModel.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DialogModel = () => {
+  return <div>DialogModel</div>;
+};
+
+export default DialogModel;
